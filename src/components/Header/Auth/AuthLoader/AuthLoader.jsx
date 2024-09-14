b55ee32624a8d0d1b5c5f97a0b3e3977538c18d9
Niakrist <ak_88dron@inbox.ru>
2024-09-14 13:56:47 +0300

    fix AuthLoader

--- src/components/Header/Auth/AuthLoader/AuthLoader.jsx
+++ src/components/Header/Auth/AuthLoader/AuthLoader.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import { PuffLoader } from "react-spinners";
 
 const AuthLoader = () => {
-  return <PuffLoader color="#cc6633" />;
+  return <PuffLoader color="#cc6633" css={{ display: "block" }} size="30" />;
 };
 
 export default AuthLoader;
